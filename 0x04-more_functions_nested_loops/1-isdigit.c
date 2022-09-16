32880c1fc8f0bf7396b4ba2128e042d4c88022c3
Tobi Daniel Omoyeni <omoyenitobi120@gmail.com>
2022-09-16 03:35:31 -0700

    my commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,33 @@
-#include "main.h"
+#include "main.h"                                                                                       
 
-/**
-*_isdigit - checks whether a character is a digit or not
-*@c: tested character
-*Return: 1 if it is, 0 otherwise
-*/
+                                                                                                        
 
-int_isdigit(int c)
-{
-	if ((c >= 48) && (c <= 57))
-	{
-		return (1)
-	}
+/**                                                                                                     
 
-	return (0);
-}
+*_isdigit - checks whether a character is a digit or not                                                
+
+*@c: tested character                                                                                   
+
+*Return: 1 if it is, 0 otherwise                                                                        
+
+*/                                                                                                      
+
+                                                                                                        
+
+int_isdigit(int c)                                                                                      
+
+{                                                                                                       
+
+        if ((c >= 48) && (c <= 57))                                                                     
+
+        {                                                                                               
+
+                return (1)                                                                              
+
+        }                                                                                               
+
+                                                                                                        
+
+        return (0);                                                                                     
+
+}           
