443b704c1e145cd475fcf5ed2b469f7fa6a7cd91
Tobi Daniel Omoyeni <omoyenitobi120@gmail.com>
2022-09-17 02:28:26 -0700

    My commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,39 +1,47 @@
-#include <stdio>                                                                                                                  
+#include <stdio>                                                                                                                
 
-#include "main.h"                                                                                                                 
+#include "main.h"                                                                                                               
 
-                                                                                                                                  
+                                                                                                                                
 
-/**                                                                                                                               
+/**                                                                                                                             
 
- * print_numbers - functions that print numbers from                                                                              
+ * print_numbers - functions that print numbers from                                                                            
 
- * 0 to 9                                                                                                                         
+ * 0 to 9                                                                                                                       
 
- *                                                                                                                                
+ *                                                                                                                              
 
- * Return: returns nothing                                                                                                        
+ * Return: returns nothing                                                                                                      
 
- * /                                                                                                                              
+ */                                                                                                                             
 
-                                                                                                                                  
+                                                                                                                                
 
- void print_numbers(void)                                                                                                         
+ *void print_numbers(void)                                                                                                      
 
- {                                                                                                                                
+ {                                                                                                                              
 
-        int n;                                                                                                                    
+                                                                                                                                
 
-                                                                                                                                  
+        int n;                                                                                                                  
 
-        for (n = 48; n < 58; n++)                                                                                                 
+                                                                                                                                
 
-        {                                                                                                                         
+        for (n = 48; n < 58; n++)                                                                                               
 
-                putchar(n);                                                                                                       
+                                                                                                                                
 
-        }                                                                                                                         
+        {                                                                                                                       
 
-        putchar(10);                                                                                                              
+                                                                                                                                
 
-}
+                                                                                                                                
+
+                putchar(n);                                                                                                     
+
+        }                                                                                                                       
+
+        putchar(10);                                                                                                            
+
+} 
