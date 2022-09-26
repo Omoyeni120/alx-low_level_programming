4a0fc523ecbc147e6174d0becabe98904a1e750b
Tobi Daniel Omoyeni <omoyenitobi120@gmail.com>
2022-09-26 06:47:21 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,34 @@
+#include <stdio.h>                                                                                                                      
+
+                                                                                                                                        
+
+/**                                                                                                                                     
+
+ *_memset - fills memory with a constant byte                                                                                           
+
+ *@s: memory space to be filled                                                                                                         
+
+ *@b: byte to fill with                                                                                                                 
+
+ *@n: number of space to fill                                                                                                           
+
+ *                                                                                                                                      
+
+ * *Return: pointer ton memory area s                                                                                                   
+
+ */                                                                                                                                     
+
+char*_memset(char*s,charb, unsigned int n)                                                                                              
+
+{                                                                                                                                       
+
+unsigned int i;                                                                                                                         
+
+for (i = 0; i  <n ; i++)                                                                                                                
+
+{                                                                                                                                       
+
+return (s);                                                                                                                             
+
+}                                                                                                                                       
+
